0ab5e6357e6e8741879f670766a1d965e7dcaf22
Murali Krishna <vmurali1444@gmail.com>
2016-04-15 23:44:35 +0530

    Added comments for how to run the tests in chrome driver

--- src/example/witl/Browser.java
+++ src/example/witl/Browser.java
@@ -3,11 +3,14 @@ package example.witl;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Browser {
     public String loginToFacebook(String username, String password) throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
+//        System.setProperty("webdriver.chrome.driver", "/path/to/the/downloaded/chromedriver");
+//        WebDriver driver = new ChromeDriver();
         String basePage = "https://www.facebook.com";
         driver.get(basePage);
 
